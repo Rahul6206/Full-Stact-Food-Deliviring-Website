9022dd0e4b61c8eb96753959b460b060b3899693
Rahul Kumar <rahulrp8677@gmail.com>
2025-09-01 23:17:15 +0530

    SingIN controllor added

--- Backend-Server/controllers/auth.controller.js
+++ Backend-Server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model";
 import Token from "../utils/token";
-const Singup = async (req, res) => {
+import bcrypt from "bcryptjs";
+export const Singup = async (req, res) => {
     try {
         const { fullname, email, password, contactnumber, role } = req.body;
         const user = await User.findOne({ $or: [{ email }, { contactnumber }] });
@@ -25,10 +26,41 @@ const Singup = async (req, res) => {
             sameSite:"strict",
             maxAge:24*60*60*1000
         })
-        await newUser.save();
-        res.status(201).json({ message: "User registered successfully", user: { id: newUser._id, fullname: newUser.fullname, email: newUser.email, contactnumber: newUser.contactnumber, role: newUser.role }, token });
+        // await newUser.save();
+       return res.status(201).json({ message: "User registered successfully", user: { id: newUser._id, fullname: newUser.fullname, email: newUser.email, contactnumber: newUser.contactnumber, role: newUser.role }, token });
         // res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
+        console.log("Singup error is: ", error);
+        
+        res.status(500).json({ message: "Server error", error: error.message });
+
+    }
+}
+export const Singin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne(email);
+        if (!user) {
+            return res.status(400).json({ message: "User not exists" });
+        }
+        
+        const decpassword=await bcrypt.compare(password,user.password);
+        if(!decpassword){
+            return res.status(400).json({ message: "Invalid Password" });
+        }
+        const token= Token(newUser._id);
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV==="production",
+            sameSite:"strict",
+            maxAge:24*60*60*1000
+        })
+        // await newUser.save();
+       return res.status(20).json({ message: "User Login successfully", user});
+        // res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.log("Singin error is: ", error);
+        
         res.status(500).json({ message: "Server error", error: error.message });
 
     }
