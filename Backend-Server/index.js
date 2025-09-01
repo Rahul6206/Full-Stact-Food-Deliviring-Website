002eb70d9000e0e14ecf061f47ed5639bede3bc1
Rahul Kumar <rahulrp8677@gmail.com>
2025-09-01 23:37:40 +0530

    Cors set

--- Backend-Server/index.js
+++ Backend-Server/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
 import ConnectMongoDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './routes/auth.route.js';
+
+// Load environment variables
 const app = express();
 const PORT = process.env.port || 5000; 
 
+
 // Middleware
 app.use(express.json());
-
+app.use(cookieParser)
+app.use('/api/auth',authRouter);
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Food Delivery Backend Server!');
