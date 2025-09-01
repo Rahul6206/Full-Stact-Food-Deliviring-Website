c6968c24771a000e08391ce7e64a3eaa29e3dbdf
Rahul Kumar <rahulrp8677@gmail.com>
2025-09-01 22:58:23 +0530

    some minor changes

--- Backend-Server/utils/token.js
+++ Backend-Server/utils/token.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const Token=async (userID)=>{
     try{
-        const token=await jwt.sign({userID},process.env.JWT_SECRET,{expiresIn:'1d'});
+        const token=jwt.sign({userID},process.env.JWT_SECRET,{expiresIn:'1d'});
         return token; 
     }catch(error){
         throw new Error('Token generation failed');
