e6cebaa77bb9b21168333e940f80869102280d3f
Rahul Kumar <rahulrp8677@gmail.com>
2025-10-13 23:14:10 +0530

    Adding Toster form sonner for better pop UP

--- Frontend-UI/src/App.jsx
+++ Frontend-UI/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Route, Routes} from 'react-router-dom'
+import { Toaster } from 'sonner'
+import SignUp from './Pages/Singup'
+import Login from './Pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-   
+    <Toaster richColors position="top-center" />
+   <Routes>
+    <Route path='/' element={<h1>Home Page</h1>}/>
+    <Route path='/singup' element={<SignUp/>}/>
+    <Route path='/singin' element={<Login/>}/>
+   </Routes>
     </>
   )
 }
