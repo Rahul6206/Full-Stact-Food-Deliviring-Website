eb8bc1d42a27d4b992a4b1b4d9be5b4c32bcfda7
Rahul Kumar <rahulrp8677@gmail.com>
2025-10-15 02:41:28 +0530

    add nodmailer for send mail

---
+++ Backend-Server/utils/mail.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure:true,
+  port:465,
+  auth: {
+    user: process.env.Email,
+    pass: process.env.GOOGLE_APP_PASSWORD,
+  },
+});
+export const sendOtp=async (to,otp) => {
+  const info = await transporter.sendMail({
+    from: process.env.Email,
+    to: to,
+    subject: "Reset Password",
+    text: "Hello world?", // plain‑text body
+    html: `<b>Your Otp is <h1>${otp}</h1> <p>It expires is 60 sec</p> </b>`, // HTML body
+  });
+
+  console.log("Message sent:", info.messageId);
+};
