58cf466eedbed64baf911f5e6757bfa227244787
Rahul Kumar <rahulrp8677@gmail.com>
2025-10-15 03:16:47 +0530

    Reset password are set

--- Backend-Server/routes/auth.route.js
+++ Backend-Server/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { Signout, Singin, Singup } from '../controllers/auth.controller.js';
+import { Signout, Singin, Singup,sendotp,verifiedOTP,Reserpassword } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup',Singup);
 authRouter.post('/signin',Singin);
-authRouter.post('/signout',Signout);
+authRouter.get('/signout',Signout);
+authRouter.post('/send-otp',sendotp);
+authRouter.post('/Reset-password',Reserpassword);
+authRouter.post('/verifiy-otp',verifiedOTP);
 
 export default authRouter;
