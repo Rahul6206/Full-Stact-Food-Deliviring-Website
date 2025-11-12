1bf461385f9f6b17209cb5082d8aa07e571b0761
Rahul Kumar <rahulrp8677@gmail.com>
2025-11-13 02:02:16 +0530

    Owner dahsboard are add but not functionality added only ui added

--- Frontend-UI/src/Pages/Home.jsx
+++ Frontend-UI/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import NavBar from '../Components/NavBar';
+import OwnerDashboard from '../Components/OwnerDashboard';
 
 const Home = () => {
   const { Userdata } = useSelector(state => state.user);
@@ -9,7 +10,10 @@ const Home = () => {
     <div className='w-screen h-screen bg-red-300 '>
 
       {Userdata?.role === "Owner" && (
+        <>
         <NavBar/>
+        <OwnerDashboard/>
+        </>
       )}
       {Userdata?.role === "user" && (
         <h1 className='text-3xl font-bold text-center pt-40'>Welcome to User Home Page</h1>
