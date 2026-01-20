f7beb3013e2f4d7eb14e5437b27717d20acea2d6
Rahul Kumar <rahulrp8677@gmail.com>
2026-01-20 23:03:28 +0530

    Adding Raiting and number of user rate us

--- Backend-Server/models/shop.model.js
+++ Backend-Server/models/shop.model.js
@@ -2,45 +2,55 @@ import mongoose from "mongoose";
 
 const ShopSchema = new mongoose.Schema({
 
-name:{
-    type:String,
-    required:true,
+    name: {
+        type: String,
+        required: true,
 
-},
-image:{
-    type:String,
-    
-    
-},
-ownerId:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'User',
-    required:true,
-},
-
-city:{
-    type:String,
-    required:true,
-    
-},
-state:{
-    type:String,
-    required:true,
-    
-},
-address:{
-    type:String,
-    required:true,
-    
-},
-Items:{
- type: mongoose.Schema.Types.ObjectId,
- ref:'Items'
-}
+    },
+    image: {
+        type: String,
 
 
-},{timestamps:true})
+    },
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
 
-const  Shopmodel= mongoose.model('Shop',ShopSchema);
+    city: {
+        type: String,
+        required: true,
+
+    },
+    state: {
+        type: String,
+        required: true,
+
+    },
+    address: {
+        type: String,
+        required: true,
+
+    },
+    Items: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Items'
+    },
+    Rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    NouR: {
+        type: Number,
+        required: true,
+        default: 0,
+    }
+
+
+}, { timestamps: true })
+
+const Shopmodel = mongoose.model('Shop', ShopSchema);
 
 export default Shopmodel;
